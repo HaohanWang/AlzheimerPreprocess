4b11ebebe975530f0ede3b3da6d9ccdc77d4a35c
HaohanWang <haohanw@andrew.cmu.edu>
2016-08-04 13:42:12 -0400

    snp subset

--- generateData/traitSubset.py
+++ generateData/traitSubset.py
@@ -1,8 +1,7 @@
 __author__ = 'Haohan Wang'
 
-subsetIndex = [1, 2, 4, 5, 6]
-
 def getSubset():
+    subsetIndex = [1, 2, 4, 5, 6]
     traits = [line.strip().split(',') for line in open('traits.csv')]
     traits_d = [line.strip() for line in open('traits_d.csv')]
 
@@ -24,6 +23,23 @@ def getSubset():
     f2 = open('traits_d_sub.csv', 'w')
     for s in subsetIndex:
         f2.writelines(traits_d[s]+'\n')
+    f2.close()
+
+def getSubsetSNP():
+    snps = [line.strip().split(',') for line in open('snps.csv')]
+    markers = [line.strip() for line in open('markers.csv')]
+
+    snps_new = []
+    for line in snps:
+        snps_new.append(line[:100])
+    f1 = open('snps_sub.csv', 'w')
+    for line in snps_new:
+        f1.writelines(','.join(line)+'\n')
+    f1.close()
+
+    f2 = open('markers_sub.csv', 'w')
+    for i in range(100):
+        f2.writelines(markers[i]+'\n')
     f2.close()
 
 if __name__ == '__main__':
