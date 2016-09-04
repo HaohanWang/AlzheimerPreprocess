af9d2a46d34a0fb4a8387cbc9697f9dd5475844d
HaohanWang <haohanw@andrew.cmu.edu>
2016-09-04 19:58:42 -0400

    check

--- plinkTransform/checkPhenotype.py
+++ plinkTransform/checkPhenotype.py
@@ -4,12 +4,15 @@ def checkMapFile(gpath):
     text = [line.strip() for line in open(gpath)][1:]
     marker = []
     count = 0
+    c = 0
     for line in text:
         items = line.split()
         marker.append((items[3], items[1], '0', items[-1]))
-        if items[3] == '0':
+        if items[3] == 'Unspecified':
             print line, count
+            c += 1
         count += 1
+    print 'total', c
     # f1 = open('snps.map', 'w')
     # for a in marker:
     #     f1.writelines('\t'.join(a) + '\n')
