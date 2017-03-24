a62bd3319149ea9c3cd0ba1480ce4fe34b4d314b
HaohanWang <haohanw@andrew.cmu.edu>
2017-03-24 10:53:24 -0400

    code update

--- cleanTraits/csv2npy.py
+++ cleanTraits/csv2npy.py
@@ -2,8 +2,8 @@ __author__ = 'Haohan Wang'
 
 import numpy as np
 
-snps = np.loadtxt('../cleanedData/all/marker value.csv', delimiter=',')
-np.save('../cleanedData/all/snps', snps)
+# snps = np.loadtxt('../cleanedData/all/marker value.csv', delimiter=',')
+# np.save('../cleanedData/all/snps', snps)
 
 phenos = np.loadtxt('../cleanedData/all/traits value.csv', delimiter=',')
-np.save('../cleanedData/all/phenos', phenos)
+np.save('../cleanedData/all/phenosMulti', phenos)
